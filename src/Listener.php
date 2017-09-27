79cd376ba0293d9f66857f7d2ddb08c4d22db75e
Sergey Kozyakov <koziakov@ya.ru>
2017-09-27 23:08:25 +0500

    Unicode symbols fixed

--- src/Listener.php
+++ src/Listener.php
@@ -39,20 +39,21 @@ class Listener
                 $chat_id = $result['message']['chat']['id'];
                 $user_name = $result['message']['from']['username'];
                 
-                $keyboard = [['\xE2\x9E\xA1 Помощь']];
+                $keyboard = [["\xE2\x9E\xA1 Старт"], ["\xE2\x9E\xA1 Помощь"]];
                 
                 $answer = '';
                 $reply_markup = null;
 
                 switch ($text) {
                     case '/start':
+                    case "\xE2\x9E\xA1 Старт":
                         $answer = 'Привет, '.$user_name.'! Я SkoobyBot. Как дела?';
                         $reply_markup = $this->getTelegram()->replyKeyboardMarkup(
                             ['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]
                         );
                         break;
                     case '/help':
-                    case '\xE2\x9E\xA1 Помошь':
+                    case "\xE2\x9E\xA1 Помошь":
                         $answer = 'Смотри, основные команды — это /start и /help и пока этого достаточно. В принципе, можно любой текст и картинку мне отправить. Увидишь, что будет.';
                         break;
                     default:
