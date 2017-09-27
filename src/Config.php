f354146c982df30bd79699d6cf4d0306fb249cd5
Sergey Kozyakov <koziakov@ya.ru>
2017-09-27 23:54:46 +0500

    Logger lib added

--- src/Config.php
+++ src/Config.php
@@ -3,9 +3,14 @@ namespace SkoobyBot;
 
 class Config
 {
+    const logDir = '../logs';
     const telegramToken = '401235231:AAFQrgrtdc1MrMbhjs4f09Ug364NjvpfiPM';
     const vkToken = '6126811a6126811a6126811a45617814d1661266126811a38ed88ab48a8e8521cecb98b';
 
+    public static function getLogDir() {
+        return Config::logDir;
+    }
+    
     public static function getTelegramToken() {
         return Config::telegramToken;
     }
--- src/Listener.php
+++ src/Listener.php
@@ -3,23 +3,47 @@ namespace SkoobyBot;
 
 use SkoobyBot\Config;
 use Telegram\Bot\Api;
+use Katzgrau\KLogger\Logger;
 
 class Listener
 {
+    protected $logger = null;
     protected $telegram = null;
 
     public function __construct()
     {
-        $token = Config::getTelegramToken();
-        if (!$token) {
-            echo 'LOG: No Telegram token is specified!';
+        try {
+            $logger = new Logger(__DIR__ . '/' . Config::getLogDir());
+            $this->setLogger($logger);
+        } catch (Exception $e) {
+            echo 'SkoobyBot logger system does not work! App is stopped.';
             return;
         }
 
-        $telegram = new Api($token);
-        $this->setTelegram($telegram);
+        $token = Config::getTelegramToken();
+        if (!$token) {
+            $this->getLogger()->error('No Telegram token is specified!');
+            return;
+        }
+
+        try {
+            $telegram = new Api($token);
+            $this->setTelegram($telegram);
+        } catch (Exception $e) {
+            $this->getLogger()->error('Telegram API connection error!');
+            return;
+        }
     }
 
+    public function getLogger() {
+        return $this->logger;
+    }
+
+    public function setLogger($logger) {
+        $this->logger = $logger;
+        return $this;
+    }
+    
     public function getTelegram() {
         return $this->telegram;
     }
@@ -31,44 +55,44 @@ class Listener
 
     public function getUpdates()
     {
-        if ($this->getTelegram()) {
-            $result = $this->getTelegram()->getWebhookUpdates();
+        if (!$this->getTelegram()) {
+            $this->getLogger()->error('Cannot receive user message until connection is created!');
+            return;
+        }
+        
+        $result = $this->getTelegram()->getWebhookUpdates();
             
-            if ($result && isset($result['message'])) {
-                $text = $result['message']['text'];
-                $chat_id = $result['message']['chat']['id'];
-                $user_name = $result['message']['from']['username'];
-                
-                $keyboard = [["\xE2\x9E\xA1 Старт"], ["\xE2\x9E\xA1 Помощь"]];
-                
-                $answer = '';
-                $reply_markup = null;
+        if ($result && isset($result['message'])) {
+            $text = $result['message']['text'];
+            $chat_id = $result['message']['chat']['id'];
+            $user_name = $result['message']['from']['firstname'];
+            
+            $keyboard = [["\xE2\x9E\xA1 Помощь"]];
+            
+            $answer = '';
+            $reply_markup = null;
 
-                switch ($text) {
-                    case '/start':
-                    case "\xE2\x9E\xA1 Старт":
-                        $answer = 'Привет, '.$user_name.'! Я SkoobyBot. Как дела?';
-                        $reply_markup = $this->getTelegram()->replyKeyboardMarkup(
-                            ['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]
-                        );
-                        break;
-                    case '/help':
-                    case "\xE2\x9E\xA1 Помошь":
-                        $answer = 'Смотри, основные команды — это /start и /help и пока этого достаточно. В принципе, можно любой текст и картинку мне отправить. Увидишь, что будет.';
-                        break;
-                    default:
-                        $answer = 'Я получил твоё сообщение и рассмотрю его :-)';
-                        break;
-                }
+            switch ($text) {
+                case '/start':
+                case "\xE2\x9E\xA1 Старт":
+                    $answer = 'Привет, '.$user_name.'! Я SkoobyBot. Как дела?';
+                    $reply_markup = $this->getTelegram()->replyKeyboardMarkup(
+                        ['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]
+                    );
+                    break;
+                case '/help':
+                case "\xE2\x9E\xA1 Помощь":
+                    $answer = 'Смотри, основные команды — это /start и /help и пока этого достаточно. В принципе, можно любой текст и картинку мне отправить. Увидишь, что будет.';
+                    break;
+                default:
+                    $answer = 'Я получил твоё сообщение и рассмотрю его :-)';
+                    break;
+            }
 
-                $this->getTelegram()->sendMessage(['chat_id' => $chat_id, 'text' => $answer, 'reply_markup' => $reply_markup]);
-            }
-            else {
-                echo 'LOG: Cannot read user message!';
-            }
+            $this->getTelegram()->sendMessage(['chat_id' => $chat_id, 'text' => $answer, 'reply_markup' => $reply_markup]);
         }
         else {
-            echo 'LOG: Cannot receive user message until connection is created!';
+            $this->getLogger()->error('Cannot read user message!');
         }
     }
 }
